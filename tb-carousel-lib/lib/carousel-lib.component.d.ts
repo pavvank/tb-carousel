add41656a7a8086d70fefacc4de0c539c0954bb1
Pavan_tb <pavvank@gmail.com>
2019-03-03 15:50:57 +0530

    Auto-generated commit

--- tb-carousel-lib/lib/carousel-lib.component.d.ts
+++
@@ -1,5 +0,0 @@
-import { OnInit } from '@angular/core';
-export declare class TbCarouselLibComponent implements OnInit {
-    constructor();
-    ngOnInit(): void;
-}
--- tb-carousel-lib/lib/carousel-lib.module.d.ts
+++
@@ -1,6 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-import { ComponentGenServiceConfig } from './util/component-gen.service';
-export declare class TbCarouselLibModule {
-    constructor(parentModule: TbCarouselLibModule);
-    static forRoot(config: ComponentGenServiceConfig[]): ModuleWithProviders;
-}
