7db62518e21d927c28ae5be83e608daa1b4747e5
Pavan_tb <pavvank@gmail.com>
2019-03-03 22:34:20 +0530

    Auto-generated commit

--- tb-carousel-lib/lib/util/component-gen-factory.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { ComponentFactoryResolver } from '@angular/core';
-import { ComponentGenService, ComponentGenServiceConfig } from './component-gen.service';
-export declare class ComponentGenFactoryService {
-    private resolver;
-    private config;
-    constructor(resolver: ComponentFactoryResolver, config: ComponentGenServiceConfig);
-    createComponentGenServiceObject(componentId: number): ComponentGenService;
-}
--- tb-carousel-lib/public_api.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/carousel-lib.service';
-export * from './lib/carousel-lib.component';
-export * from './lib/carousel-lib.module';
